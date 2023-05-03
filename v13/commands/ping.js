18f13c975804fcec681751de527e8a1f633671c6
たいやき村長(TaiyakiSontyo) <95021926+TaiyakiSontyo@users.noreply.github.com>
2023-05-03 22:11:49 +0900

    Update ping.js

--- v13/commands/ping.js
+++ v13/commands/ping.js
@@ -3,11 +3,11 @@ module.exports = {
         name: "ping",
         description: "pingを送信します。",
     },
-	async execute(interaction) {
-        const now = Date.now();
-		await interaction.deferReply();
-        const sent = await interaction.channel.send('Pinging...');
-        await interaction.deleteReply();
-        sent.edit(`\`\`\`diff\n! Completed\n+++ ping:${now-interaction.createdTimestamp}ms +++\`\`\``);
-	}
+    async execute(interaction) {
+        const now = Date.now(); //現在時刻を取得
+	await interaction.deferReply(); //考え中...を表示
+        const sent = await interaction.channel.send('Pinging...'); //考え中...と同時にPinging...と送信
+        await interaction.deleteReply(); //考え中...を削除
+        sent.edit(`Pong!`); //Pinging...を編集し、Pong!に書き換える
+    }
 }
